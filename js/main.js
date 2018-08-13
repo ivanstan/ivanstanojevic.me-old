5186eedeab3ed3d0135ee501c6e07e34e46d4229
Ivan Stanojevic <ivan@byteout.com>
2018-08-13 12:14:52 +0200

    space apps portfolio

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
 $(function() {
     $(document).ready(function () {
-        setTimeout(function(){
-            $('.loading').remove();
-        }, 1000);
+        $('img').each(function (index, element) {
+            let image = new Image()
+            image.src = $(element).attr('src')
+        })
+
+        $('.loading').remove()
     });
 
-    $(window).scroll(function() {
-        if ($(".navbar").offset().top > 50) {
-            $(".navbar-fixed-top").addClass("top-nav-collapse");
-        } else {
-            $(".navbar-fixed-top").removeClass("top-nav-collapse");
-        }
-    });
+    let navBar = $('.navbar')
+    let navBarFixed = $('.navbar-fixed-top')
+
+    if (navBar.length > 0 && navBarFixed.length > 0) {
+        $(window).scroll(function() {
+            if (navBar.offset().top > 50) {
+                navBarFixed.addClass('top-nav-collapse')
+            } else {
+                navBarFixed.removeClass('top-nav-collapse')
+            }
+        });
+    }
 
     $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
+        let $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
